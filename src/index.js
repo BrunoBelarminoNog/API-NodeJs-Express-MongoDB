bfcd0c48c509a1c6e87d6a2106f33388267a89e6
Bruno Belarmino <brunobelarm.nog@gmail.com>
2021-01-12 21:48:40 -0300

    Autenticação da API

--- src/index.js
+++ src/index.js
@@ -8,5 +8,11 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 
 require('./controllers/authController')(app);
+require('./controllers/projectController')(app);
 
-app.listen(3000);
+
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+    console.log('RESTful API server started on: ' + port);
+});
